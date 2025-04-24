771ef1182465f28c8f9f9c5489ab780884c85a66
daeun <5ekdmsdldl@gmail.com>
2025-04-24 23:52:48 +0900

    update init install

--- init_install.bat
+++ init_install.bat
@@ -1,4 +1,9 @@
+python -m venv .venv
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+.venv\Scripts\activate.bat
+
 pip install selenium
 pip install customtkinter
 pip install undetected_chromedriver
 pip install pyinstaller
+pip install setuptools
